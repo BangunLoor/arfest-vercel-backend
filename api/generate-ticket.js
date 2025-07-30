a9e87633b95fb04468326eeb1502a4d7cacca1dc
BangunLoor <Bangunloor@gmail.com>
2025-07-30 17:30:53 +0700

    Update generate-ticket.js

--- api/generate-ticket.js
+++ api/generate-ticket.js
@@ -1,18 +1,9 @@
+
 import sendEmail from '../utils/sendEmail.js';
 import generatePDF from '../utils/generatePDF.js';
 import fs from 'fs';
 
 export default async function handler(req, res) {
-  // ✅ Tambahan untuk fix CORS
-  if (req.method === 'OPTIONS') {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    return res.status(200).end();
-  }
-
-  res.setHeader('Access-Control-Allow-Origin', '*'); // ganti * dengan domain Netlify kamu jika ingin lebih aman
-
   if (req.method !== 'POST') return res.status(405).end();
 
   const { name, email, kontak, harga } = req.body;
